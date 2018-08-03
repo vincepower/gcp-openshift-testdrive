af06d5d3f66a8dc43bed61f190335ee2529f0e3e
Vince Power <vince.power@gmail.com>
2018-08-03 01:44:42 -0300

    Just cleaning up the preparation scripts

---
+++ prepare-initial-images/scripts/initial-setup.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+##
+## RUN AS ROOT
+##
+## This script will take care of setting up ansible.cfg, ssh, and docker
+##
+## This script requires a golden image of RHEL to run on that is
+## subscribed to RHSM or a Satelite Server.
+##
+
+## Adding Google's Cloud repositories that will be used later
+echo "
+[google-cloud-compute]
+name=Google Cloud Compute
+baseurl=https://packages.cloud.google.com/yum/repos/google-cloud-compute-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+
+[google-cloud-sdk]
+name=Google Cloud SDK
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+" > /etc/yum.repos.d/google-cloud.repo
+
+## Exposing exclusioned packages
+yum install -y atomic-openshift-excluder atomic-openshift-docker-excluder
+atomic-openshift-excluder unexclude
+
+## Installing required packages
+yum install -y google-cloud-sdk
+yum install -y wget git net-tools bind-utils yum-utils iptables-services bridge-utils
+yum install -y bash-completion kexec-tools sos psacct docker
+yum install -y cloud-utils-growpart ansible glusterfs-fuse
+
+## Updating all packages
+yum update -y
+
+## Resizing base disk to 80GB
+# gcloud command is here for reference as it is run as part of the start-up script
+#gcloud -q compute disks resize --zone {{ properties["zone"] }} {{ diskbase }} --size 60
+growpart /dev/sda 2 -u on
+xfs_growfs /dev/sda2
+
+## Create ansible.cfg
+echo "# ansible.cfg
+[defaults]
+library        = /usr/share/ansible/openshift-ansible/roles/lib_utils/library/
+forks          = 10
+roles_path    = /etc/ansible/roles:/usr/share/ansible/roles
+host_key_checking = False
+stdout_callback = skippy
+[inventory]
+[privilege_escalation]
+[paramiko_connection]
+pty=False
+[ssh_connection]
+control_path = %(directory)s/%%h-%%r
+[persistent_connection]
+[accelerate]
+[selinux]
+[colors]
+[diff]
+" > /etc/ansible/ansible.cfg
+
+## Create ansible hosts
+echo "
+[OSEv3:vars]
+# OpenShift version
+openshift_release=v3.10
+deployment_type=openshift-enterprise
+
+# Default console port (works best when they match)
+openshift_master_api_port=443
+openshift_master_console_port=443
+
+# System Settings
+ansible_ssh_user=root
+openshift_override_hostname_check=true
+openshift_use_dnsmasq=true
+openshift_install_examples=true
+openshift_disable_check=memory_availability,docker_image_availability
+
+# Identity
+openshift_master_identity_providers=[{'name': 'htpasswd_auth', 'login': 'true', 'challenge': 'true', 'kind': 'HTPasswdPasswordIdentityProvider'}]
+
+# Disable Metrics
+openshift_metrics_install_metrics=false
+
+# Disable Logging
+openshift_logging_install_logging=false
+
+# General GlusterFS Storage
+openshift_storage_glusterfs_namespace=app-storage
+openshift_storage_glusterfs_storageclass=true
+openshift_storage_glusterfs_storageclass_default=false
+openshift_storage_glusterfs_block_deploy=true
+openshift_storage_glusterfs_block_host_vol_size=100
+openshift_storage_glusterfs_block_storageclass=true
+openshift_storage_glusterfs_block_storageclass_default=false
+
+# This will change every deployment of a Test Drive
+openshift_master_default_subdomain=apps.DEPLOYMENTNAME.gcp.testdrive.openshift.com
+openshift_master_cluster_public_hostname=master.DEPLOYMENTNAME.gcp.testdrive.openshift.com
+openshift_master_cluster_public_vip=MASTEREXTERNALIP
+
+[OSEv3:children]
+etcd
+masters
+nodes
+glusterfs
+
+[masters]
+MASTERNODE
+
+[glusterfs]
+# General Storage
+APPNODE1 glusterfs_devices='["/dev/sdc"]'
+APPNODE2 glusterfs_devices='["/dev/sdc"]'
+APPNODE3 glusterfs_devices='["/dev/sdc"]'
+
+[etcd]
+MASTERNODE
+
+[nodes]
+MASTERNODE openshift_node_group_name='node-config-master'
+INFRANODE openshift_node_group_name='node-config-infra'
+APPNODE1 openshift_node_group_name='node-config-compute'
+APPNODE2 openshift_node_group_name='node-config-compute'
+APPNODE3 openshift_node_group_name='node-config-compute'
+" > /etc/ansible/hosts
+
+## Create ssh_config
+echo "# ssh_config for OCP on GCP
+Host *
+    Port 22
+    Protocol 2
+    ForwardAgent no
+    ForwardX11 no
+    HostbasedAuthentication no
+    StrictHostKeyChecking no
+    Ciphers aes128-ctr,aes192-ctr,aes256-ctr,arcfour256,arcfour128,aes128-cbc,3des-cbc
+    Tunnel no
+    ServerAliveInterval 420
+" > /etc/ssh/ssh_config
+
+## Create sshd_config
+echo "# sshd_config for OCP on GCP
+HostKey /etc/ssh/ssh_host_rsa_key
+HostKey /etc/ssh/ssh_host_ecdsa_key
+HostKey /etc/ssh/ssh_host_ed25519_key
+SyslogFacility AUTHPRIV
+PermitRootLogin yes
+AuthorizedKeysFile .ssh/authorized_keys
+PasswordAuthentication no
+ChallengeResponseAuthentication no
+UsePAM yes
+AllowTcpForwarding yes
+X11Forwarding no
+ClientAliveInterval 180
+AcceptEnv LANG LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_MESSAGES
+AcceptEnv LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT
+AcceptEnv LC_IDENTIFICATION LC_ALL LANGUAGE
+AcceptEnv XMODIFIERS
+Subsystem sftp  /usr/libexec/openssh/sftp-server
+" > /etc/ssh/sshd_config
+
+## Restart ssh daemon and wait for a few seconds before continuing
+systemctl restart sshd
+sleep 5
+
+## Docker Storage Setup
+echo "STORAGE_DRIVER=overlay2
+DEVS=/dev/sdb
+VG=docker-pool" > /etc/sysconfig/docker-storage-setup
+
+## Run the setup
+docker-storage-setup
+
+## Enable Docker Service
+systemctl enable docker
+
+## Start Docker Service
+systemctl start docker
+
---
+++ prepare-initial-images/scripts/install-openshift-bits.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+##
+## RUN AS ROOT
+##
+## Run this after setting up docker storage and registering with RHSM
+## Script to download all the OpenShift bits we'll ever need (hopefully)
+##
+
+## Version of OpenShift
+VERSION=v3.10.14
+
+## Make go's home directory
+mkdir -p /root/go/bin
+echo "export PATH=\$PATH:/root/go/bin" >> /root/.bashrc
+
+## Starting docker daemon
+systemctl start docker
+
+## Installing OpenShift rpms
+yum install -y openshift-ansible* atomic* *-openshift-* google-cloud-sdk*
+
+## Downloading all the containers
+docker pull registry.access.redhat.com/cloudforms46/cfme-httpd-configmap-generator
+docker pull registry.access.redhat.com/cloudforms46/cfme-openshift-app
+docker pull registry.access.redhat.com/cloudforms46/cfme-openshift-app-ui
+docker pull registry.access.redhat.com/cloudforms46/cfme-openshift-embedded-ansible
+docker pull registry.access.redhat.com/cloudforms46/cfme-openshift-httpd
+docker pull registry.access.redhat.com/cloudforms46/cfme-openshift-memcached
+docker pull registry.access.redhat.com/cloudforms46/cfme-openshift-postgresql
+docker pull registry.access.redhat.com/jboss-amq-6/amq63-openshift
+docker pull registry.access.redhat.com/jboss-datagrid-7/datagrid71-client-openshift
+docker pull registry.access.redhat.com/jboss-datagrid-7/datagrid71-openshift
+docker pull registry.access.redhat.com/jboss-datavirt-6/datavirt63-driver-openshift
+docker pull registry.access.redhat.com/jboss-datavirt-6/datavirt63-openshift
+docker pull registry.access.redhat.com/jboss-decisionserver-6/decisionserver64-openshift
+docker pull registry.access.redhat.com/jboss-eap-6/eap64-openshift
+docker pull registry.access.redhat.com/jboss-eap-7/eap70-openshift
+docker pull registry.access.redhat.com/jboss-processserver-6/processserver64-openshift
+docker pull registry.access.redhat.com/jboss-webserver-3/webserver30-tomcat7-openshift:1.1
+docker pull registry.access.redhat.com/jboss-webserver-3/webserver30-tomcat7-openshift:latest
+docker pull registry.access.redhat.com/jboss-webserver-3/webserver31-tomcat7-openshift
+docker pull registry.access.redhat.com/jboss-webserver-3/webserver31-tomcat8-openshift
+docker pull registry.access.redhat.com/openshift3/apb-base:$VERSION
+docker pull registry.access.redhat.com/openshift3/apb-tools:$VERSION
+docker pull registry.access.redhat.com/openshift3/csi-attacher:$VERSION
+docker pull registry.access.redhat.com/openshift3/csi-driver-registrar:$VERSION
+docker pull registry.access.redhat.com/openshift3/csi-livenessprobe:$VERSION
+docker pull registry.access.redhat.com/openshift3/csi-provisioner:$VERSION
+docker pull registry.access.redhat.com/openshift3/efs-provisioner:$VERSION
+docker pull registry.access.redhat.com/openshift3/image-inspector:$VERSION
+docker pull registry.access.redhat.com/openshift3/jenkins-1-rhel7
+docker pull registry.access.redhat.com/openshift3/jenkins-2-rhel7
+docker pull registry.access.redhat.com/openshift3/jenkins-agent-maven-35-rhel7:$VERSION
+docker pull registry.access.redhat.com/openshift3/jenkins-agent-nodejs-8-rhel7:$VERSION
+docker pull registry.access.redhat.com/openshift3/jenkins-slave-base-rhel7
+docker pull registry.access.redhat.com/openshift3/jenkins-slave-maven-rhel7
+docker pull registry.access.redhat.com/openshift3/jenkins-slave-nodejs-rhel7
+docker pull registry.access.redhat.com/openshift3/local-storage-provisioner:$VERSION
+docker pull registry.access.redhat.com/openshift3/logging-auth-proxy:$VERSION
+docker pull registry.access.redhat.com/openshift3/logging-curator:$VERSION
+docker pull registry.access.redhat.com/openshift3/logging-elasticsearch:$VERSION
+docker pull registry.access.redhat.com/openshift3/logging-eventrouter:$VERSION
+docker pull registry.access.redhat.com/openshift3/logging-fluentd:$VERSION
+docker pull registry.access.redhat.com/openshift3/logging-kibana:$VERSION
+docker pull registry.access.redhat.com/openshift3/manila-provisioner:$VERSION
+docker pull registry.access.redhat.com/openshift3/mariadb-apb:$VERSION
+docker pull registry.access.redhat.com/openshift3/mediawiki-apb:$VERSION
+docker pull registry.access.redhat.com/openshift3/metrics-cassandra:$VERSION
+docker pull registry.access.redhat.com/openshift3/metrics-hawkular-metrics:$VERSION
+docker pull registry.access.redhat.com/openshift3/metrics-hawkular-openshift-agent:$VERSION
+docker pull registry.access.redhat.com/openshift3/metrics-heapster:$VERSION
+docker pull registry.access.redhat.com/openshift3/metrics-schema-installer:$VERSION
+docker pull registry.access.redhat.com/openshift3/mysql-apb:$VERSION
+docker pull registry.access.redhat.com/openshift3/oauth-proxy:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-ansible-service-broker:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-ansible:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-cli:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-cluster-capacity:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-control-plane:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-deployer:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-descheduler:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-docker-builder:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-docker-registry:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-egress-dns-proxy:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-egress-http-proxy:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-egress-router:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-f5-router:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-haproxy-router:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-hyperkube:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-hypershift:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-keepalived-ipfailover:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-node-problem-detector:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-node:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-pod:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-recycler:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-service-catalog:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-template-service-broker:$VERSION
+docker pull registry.access.redhat.com/openshift3/ose-web-console:$VERSION
+docker pull registry.access.redhat.com/openshift3/postgresql-apb:$VERSION
+docker pull registry.access.redhat.com/openshift3/prometheus-alert-buffer:$VERSION
+docker pull registry.access.redhat.com/openshift3/prometheus-alertmanager:$VERSION
+docker pull registry.access.redhat.com/openshift3/prometheus-node-exporter:$VERSION
+docker pull registry.access.redhat.com/openshift3/prometheus:$VERSION
+docker pull registry.access.redhat.com/openshift3/registry-console:$VERSION
+docker pull registry.access.redhat.com/openshift3/snapshot-controller:$VERSION
+docker pull registry.access.redhat.com/openshift3/snapshot-provisioner:$VERSION
+docker pull registry.access.redhat.com/redhat-openjdk-18/openjdk18-openshift
+docker pull registry.access.redhat.com/redhat-sso-7/sso70-openshift
+docker pull registry.access.redhat.com/redhat-sso-7/sso71-openshift
+docker pull registry.access.redhat.com/rhel7/etcd
+docker pull registry.access.redhat.com/rhgs3/rhgs-gluster-block-prov-rhel7
+docker pull registry.access.redhat.com/rhgs3/rhgs-s3-server-rhel7
+docker pull registry.access.redhat.com/rhgs3/rhgs-server-rhel7
+docker pull registry.access.redhat.com/rhgs3/rhgs-volmanager-rhel7
+docker pull registry.access.redhat.com/rhscl/mariadb-101-rhel7
+docker pull registry.access.redhat.com/rhscl/mongodb-32-rhel7
+docker pull registry.access.redhat.com/rhscl/mysql-57-rhel7
+docker pull registry.access.redhat.com/rhscl/nodejs-6-rhel7
+docker pull registry.access.redhat.com/rhscl/perl-524-rhel7
+docker pull registry.access.redhat.com/rhscl/php-56-rhel7
+docker pull registry.access.redhat.com/rhscl/postgresql-95-rhel7
+docker pull registry.access.redhat.com/rhscl/python-35-rhel7
+docker pull registry.access.redhat.com/rhscl/ruby-24-rhel7
+
