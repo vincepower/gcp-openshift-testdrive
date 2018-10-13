b2497b3420f0b48272bf6d1163c3eb1b2f8affb4
Vince Power <vince.power@gmail.com>
2018-10-13 11:27:55 -0300

    Fine tuning 3.11 red hat prep scripts

--- prepare-initial-images/scripts/1-rhel-prep.sh
+++ prepare-initial-images/scripts/1-rhel-prep.sh
@@ -13,17 +13,20 @@
 ## 
 
 # Add correct repositories from RHSM
+echo "Disabling all repositories not required"
 subscription-manager repos --disable="*"
 
+echo "Ensuring required repositories are enabled"
 subscription-manager repos \
     --enable="rhel-7-server-rpms" \
     --enable="rhel-7-server-extras-rpms" \
-    --enable="rhel-7-server-ose-3.10-rpms" \
-    --enable="rhel-7-server-ansible-2.5-rpms" \
+    --enable="rhel-7-server-ose-3.11-rpms" \
+    --enable="rhel-7-server-ansible-2.6-rpms" \
     --enable="rhel-7-fast-datapath-rpms" \
     --enable="rh-gluster-3-client-for-rhel-7-server-rpms"
 
 ## Adding Google's Cloud repositories that will be used later
+echo "Adding Google yum repos"
 cat << EOF > /etc/yum.repos.d/google-cloud.repo
 [google-cloud-compute]
 name=Google Cloud Compute
@@ -45,22 +48,25 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
 EOF
 
 ## Exposing exclusioned packages
+echo "Installing and disabling OpenShift excluders"
 yum install -y atomic-openshift-excluder atomic-openshift-docker-excluder
 atomic-openshift-excluder unexclude
 
 ## Installing required packages
+echo "Installing prereqs"
 yum install -y wget git net-tools bind-utils yum-utils iptables-services bridge-utils
-yum install -y bash-completion kexec-tools sos psacct docker golang
-yum install -y cloud-init cloud-utils-growpart glusterfs-fuse
-yum install -y ansible
-yum install -y python-google-compute-engine google-compute-engine-oslogin google-compute-engine
-yum install -y google-cloud-sdk*
+yum install -y bash-completion kexec-tools sos psacct docker
+yum install -y cloud-init cloud-utils-growpart 
+yum install -y ansible glusterfs-fuse
 
 ## Updating all packages
+echo "Updating the OS"
 yum update -y
 
 ## Create ansible.cfg
-echo "# ansible.cfg
+echo "Updating ansible.cfg"
+cat << EOF > /etc/ansible/ansible.cfg
+# ansible.cfg
 [defaults]
 library        = /usr/share/ansible/openshift-ansible/roles/lib_utils/library/
 forks          = 10
@@ -78,9 +84,10 @@ control_path = %(directory)s/%%h-%%r
 [selinux]
 [colors]
 [diff]
-" > /etc/ansible/ansible.cfg
+EOF
 
 ## Create ansible hosts
+echo "Creating ansible/hosts"
 cat << EOF > /etc/ansible/hosts
 [OSEv3:vars]
 # OpenShift version
@@ -148,6 +155,7 @@ APPNODE3 openshift_node_group_name='node-config-compute'
 EOF
 
 ## Create ssh_config
+echo "Updating ssh_config"
 cat << EOF > /etc/ssh/ssh_config
 # ssh_config for OCP on GCP
 Host *
@@ -163,6 +171,7 @@ Host *
 EOF
 
 ## Create sshd_config
+echo "Updating sshd_config"
 cat << EOF > /etc/ssh/sshd_config
 # sshd_config for OCP on GCP
 HostKey /etc/ssh/ssh_host_rsa_key
@@ -185,21 +194,25 @@ Subsystem sftp  /usr/libexec/openssh/sftp-server
 EOF
 
 ## Restart ssh daemon and wait for a few seconds before continuing
+echo "Enabling and restarting sshd"
 systemctl restart sshd
 sleep 5
 
 ## Docker Storage Setup
+echo "Setting docker storage config"
 cat << EOF > /etc/sysconfig/docker-storage-setup
 DEVS=/dev/sdb
 VG=docker-pool
 EOF
 
 ## Make go's home directory
+echo "Creating the golang config"
 mkdir -p /root/go/bin
 echo "export GOPATH=/root/go" >> /root/.bashrc
 echo "export PATH=\$PATH:/root/go/bin" >> /root/.bashrc
 
 # Updating GRUB as per GCP's recommendations
+echo "Setting the recommended grub config"
 cat << EOF > /etc/default/grub
 GRUB_TIMEOUT=5
 GRUB_DISTRIBUTOR="$(sed 's, release .*$,,g' /etc/system-release)"
@@ -209,18 +222,24 @@ GRUB_TERMINAL_OUTPUT="console"
 GRUB_CMDLINE_LINUX="rootdelay=300 console=tty0 console=ttyS0,38400n8d net.ifnames=0"
 GRUB_DISABLE_RECOVERY="true"
 EOF
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
 
 # Creating script that can be used during an offline install
 cat << EOF > /root/get-redhat-repos.sh
 #!/bin/bash
-echo "====================================="
+
+# Unregistering from RHSM
+subscription-manager unregister
+
 # Cleaning up yum for a fresh start
 yum clean all
+
 # Getting offline repo server
-curl -o /etc/yum.repos.d/rhel7ocp311.repo http://repo.server/rhel7ocp310.repo
+curl -o /etc/yum.repos.d/rhel7ocp311.repo http://repo.server/rhel7ocp311.repo
+
 # Refreshing repository caches
 yum repolist
+
 EOF
 chmod +x /root/get-redhat-repos.sh
 
@@ -239,7 +258,12 @@ ONBOOT="yes"
 EOF
 
 # Enabling cloud-init
+echo "Enabling cloud-init"
 systemctl enable cloud-init
+
+# Installing Google Utilities
+yum install -y python-google-compute-engine google-compute-engine-oslogin google-compute-engine
+yum install -y golang google-cloud-sdk
 
 # Sending some commands
 echo ""
